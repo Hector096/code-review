38b4a846ef4c2e73470a07b184d191ec6a88cf6b
Hector <vv96rca@gmail.com>
2021-09-27 18:42:28 +0530

    code review purpose

---
+++ src/dataModel.js
@@ -0,0 +1,28 @@
+import { saveData, getData } from './localStorageHandler';
+
+export default class Task {
+  constructor(description, index, completed) {
+    this.description = description;
+    this.index = index;
+    this.completed = completed;
+  }
+
+  addNewTask() {
+    const data = getData();
+    const newTask = new Task(this.description, this.index, this.completed);
+    data.push(newTask);
+    saveData(data);
+  }
+
+  removeTask() {
+    const data = getData();
+    const index = data.findIndex((item) => item.index === this.index);
+    data.splice(index, 1);
+    let i = 1;
+    data.forEach((item) => {
+      item.index = i;
+      i += 1;
+    });
+    saveData(data);
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,123 @@
+import './style.css';
+import { isCompleted, isEdit, clearCompleted } from './isCompleted';
+import Task from './dataModel';
+import { getData } from './localStorageHandler';
+
+const element = document.getElementById('todos-list');
+const addInput = document.getElementById('add-input');
+const addTaskForm = document.getElementById('addTaskForm');
+const refreshBtn = document.getElementById('refresh');
+const clear = document.getElementById('clear-completed');
+
+const addList = (todosList) => {
+  todosList.forEach((todoItem) => {
+    const taskItem = document.createElement('div');
+    taskItem.classList.add('d-flex');
+    taskItem.classList.add('justify-content-between');
+    taskItem.classList.add('ps-3');
+    taskItem.classList.add('pe-3');
+    taskItem.classList.add('align-items-center');
+    const innerDiv = document.createElement('div');
+    innerDiv.classList.add('d-flex');
+    innerDiv.classList.add('justify-content-evenly');
+    innerDiv.classList.add('align-items-center');
+    const inputCheck = document.createElement('input');
+    inputCheck.type = 'checkbox';
+    inputCheck.classList.add('form-check-input');
+    inputCheck.id = `checkBox${todoItem.index}`;
+    const desc = document.createElement('input');
+    desc.classList.add('p-2');
+    desc.classList.add('ms-3');
+    desc.value = `${todoItem.description}`;
+    if (todoItem.completed) {
+      inputCheck.checked = true;
+      desc.style.textDecoration = 'line-through';
+      desc.style.opacity = '0.5';
+    }
+    innerDiv.appendChild(inputCheck);
+    innerDiv.appendChild(desc);
+
+    const menuButton = document.createElement('i');
+    menuButton.classList.add('fas');
+    menuButton.classList.add('fa-ellipsis-v');
+    menuButton.classList.add('text-secondary');
+    menuButton.id = `menu${todoItem.index}`;
+    const hr = document.createElement('hr');
+    taskItem.appendChild(innerDiv);
+    taskItem.appendChild(menuButton);
+    element.appendChild(taskItem);
+    element.appendChild(hr);
+
+    desc.addEventListener('change', () => {
+      isEdit(todoItem.index, desc.value.trim());
+      menuButton.classList.remove('fas');
+      menuButton.classList.remove('fa-ellipsis-v');
+      menuButton.classList.add('far');
+      menuButton.classList.add('fa-trash-alt');
+    });
+
+    desc.addEventListener('click', () => {
+      menuButton.classList.remove('fas');
+      menuButton.classList.remove('fa-ellipsis-v');
+      menuButton.classList.add('far');
+      menuButton.classList.add('fa-trash-alt');
+    });
+
+    desc.addEventListener('blur', () => {
+      menuButton.classList.add('fas');
+      menuButton.classList.add('fa-ellipsis-v');
+      menuButton.classList.remove('far');
+      menuButton.classList.remove('fa-trash-alt');
+
+      menuButton.addEventListener('click', () => {
+        const item = new Task(
+          todoItem.description,
+          todoItem.index,
+          todoItem.completed,
+        );
+        item.removeTask();
+        element.innerHTML = '';
+        addList(getData());
+      });
+    });
+
+    inputCheck.addEventListener('change', () => {
+      if (inputCheck.checked) {
+        isCompleted(todoItem.index);
+        desc.style.textDecoration = 'line-through';
+        desc.style.opacity = '0.5';
+      } else {
+        isCompleted(todoItem.index);
+        desc.style.textDecoration = 'none';
+        desc.style.opacity = '1';
+        inputCheck.checked = false;
+      }
+    });
+  });
+};
+
+clear.addEventListener('click', () => {
+  clearCompleted();
+  element.innerHTML = '';
+  addList(getData());
+});
+
+addTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (addInput.value !== null) {
+    const item = new Task(addInput.value.trim(), getData().length + 1, false);
+    item.addNewTask();
+    addInput.value = '';
+    element.innerHTML = '';
+    addList(getData());
+  }
+});
+
+refreshBtn.addEventListener('click', () => {
+  refreshBtn.classList.toggle('rotate');
+  setTimeout(() => {
+    window.location.reload();
+  }, 1000);
+});
+
+addList(getData());
